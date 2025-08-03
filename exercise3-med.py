547e2b1ca1dfdbbbdb1424fab5e65760d8d00101
hadymjd <hadimajed200@gmail.com>
2025-08-03 23:18:29 +0300

    Solved problem 3

---
+++ exercise3-med.py
@@ -0,0 +1,54 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.is_checked_out = False
+
+    def checkout(self):
+        self.is_checked_out = True
+
+    def return_book(self):
+        self.is_checked_out = False
+
+    def __str__(self):
+        return f"{self.title} by {self.author} (Checked out: {self.is_checked_out})"
+    
+class Library:
+    def __init__(self):
+        self.collection = []
+
+    def add_book(self, book):
+        self.collection.append(book)
+
+    def list_books(self):
+        for book in self.collection:
+            print(book)
+            print()
+
+    def find_book(self, title):
+        for book in self.collection:
+            if book.title.lower() == title.lower():
+                return book
+        return None
+    
+    def available_books(self):
+        for book in self.collection:
+            if not book.is_checked_out:
+                print(book)
+                print()
+
+b1 = Book("1984", "George Orwell", 1949)
+b2 = Book("The Alchemist", "Paulo Coelho", 1988)
+
+lib = Library()
+lib.add_book(b1)
+lib.add_book(b2)
+
+lib.list_books()
+
+b1.checkout()
+lib.list_books()
+
+found = lib.find_book("1984")
+print(found)
